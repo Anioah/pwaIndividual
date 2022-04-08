2f62557491d4f9c4789353c12db81ec461dce80e
Anioah <55303971+Anioah@users.noreply.github.com>
2022-04-07 20:43:13 -0500

    third chance

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var url = window.location.href; //obtenemos todo el url
-var pwaLocation = '/repositorioGitHub/sw.js'; //path donde se encuentra el sw en GitHub
+var pwaLocation = '/pwaIndividual/sw.js'; //path donde se encuentra el sw en GitHub
 
 if (navigator.serviceWorker) {
     if (url.includes('localhost')) {
