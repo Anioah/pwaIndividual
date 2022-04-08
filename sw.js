ce258e71565262e00d200202c5379bc6d941b20a
Anioah <55303971+Anioah@users.noreply.github.com>
2022-04-07 21:09:49 -0500

    third chance

--- sw.js
+++ sw.js
@@ -34,7 +34,7 @@ const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 const APP_SHELL = [
-    '/',
+    './',
     'index.html',
     'css/style.css',
     'images/icon1.ico',
